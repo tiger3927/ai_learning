a86863a5ff9f77b888deef84c6966315b9c79bc5
tiger <tiger3927@gmail.com>
2020-01-26 21:48:13 +0800

    ok

--- tiger_python_tools/test0.py
+++ tiger_python_tools/test0.py
@@ -1,6 +1,27 @@
-import os
-os.environ['KERAS_BACKEND']='mxnet'
+import numpy as np
 
-import keras
+# Compute outer product of vectors
+v = np.array([1,2,3])  # v has shape (3,)
+w = np.array([4,5])    # w has shape (2,)
+# To compute an outer product, we first reshape v to be a column
+# vector of shape (3, 1); we can then broadcast it against w to yield
+# an output of shape (3, 2), which is the outer product of v and w:
+# [[ 4  5]
+#  [ 8 10]
+#  [12 15]]
+print(v)
+print(v.shape)
+v=v.reshape(3,1)
+print(v)
+print(v * w)
 
-print(keras.backend.backend())
+# Add a vector to each row of a matrix
+v = np.array([1,2,3])
+x = np.array([[1,2,3], [4,5,6]])
+# x has shape (2, 3) and v has shape (3,) so they broadcast to (2, 3),
+# giving the following matrix:
+# [[2 4 6]
+#  [5 7 9]]
+print(x + v)
+print (x + np.reshape(w, (2, 1)))
+print(x*2)
